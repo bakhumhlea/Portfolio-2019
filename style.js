4780b27d5e94c90b0877eaffdeb88ff2cb879fe6
Thanaphon Chaysawat <23chaysawat@Thanaphons-MacBook-Pro.local>
2018-11-27 12:36:16 -0800

    edit animation and iphone6-7-8 responsive

--- style.js
+++ style.js
@@ -56,6 +56,7 @@ var orbitSeven = document.getElementById("orbit-7");
 var orbitEight = document.getElementById("orbit-8");
 var orbitNine = document.getElementById("orbit-9");
 var orbitFront = document.getElementById("front");
+var center = document.getElementById("center");
 
 var polyOne = document.getElementById("polygon-1");
 var polyFront = document.getElementById("polygon-front");
@@ -63,42 +64,48 @@ var polyFront = document.getElementById("polygon-front");
 var pauseButton = document.getElementById("pause");
 var pauseInfo = document.getElementById("pause-info");
 
+var path = document.getElementById("timer-bar");
+
 var timer;
+var freezed = false;
+
+function handleMouseMove(event) {
+  clearInterval(timer);
+  pauseInfo.textContent = "click to freeze";
+
+  event = event || window.event;
+
+  var offX = 50 - (50*event.clientX)/(window.innerWidth/2);
+  var offY = 50 - (50*event.clientY)/(window.innerHeight/2);
+
+  orbitOne.setAttribute("style", `left: ${50 - offX*1}%; top: ${50 - offY*1}%;`);
+  orbitTwo.setAttribute("style", `left: ${50 - offX*3}%; top: ${50 - offY*3}%;`);
+  orbitThree.setAttribute("style", `left: ${50 - offX*5}%; top: ${50 - offY*5}%;`);
+  orbitFour.setAttribute("style", `left: ${50 - offX*7}%; top: ${50 - offY*7}%;`);
+  orbitFive.setAttribute("style", `left: ${50 - offX*6}%; top: ${50 - offY*6}%;`);
+  orbitSix.setAttribute("style", `left: ${50 - offX*4}%; top: ${50 - offY*4}%;`);
+  orbitSeven.setAttribute("style", `left: ${50 - offX*5.5}%; top: ${50 - offY*5.5}%;`);
+  orbitEight.setAttribute("style", `left: ${50 - offX*8}%; top: ${50 - offY*8}%;`);
+  orbitNine.setAttribute("style", `left: ${50 - offX*(-1)}%; top: ${50 - offY*(-1)}%;`);
+
+  polyOne.setAttribute("style", `left: ${50 - offX*5.5}%; top: ${50 - offY*5.5}%;`)
+  polyFront.setAttribute("style", `left: ${50 - offX*9.5}%; top: ${50 - offY*9.5}%;`)
+
+  orbitFront.setAttribute("style", `left: ${50 - offX*9}%; top: ${50 - offY*9}%;`);
+
+  posX.textContent = `_${event.clientX}`;
+  posY.textContent = `_${event.clientY}`;
+
+  path.style.stroke = "rgb(97, 173, 235)";
+  path.setAttribute("d", `M0 2 L0 2`);
+}
 
 targetObject.onmouseover = function trigger() {
-  var path = document.getElementById("timer-bar");
   showInfo();
-
-  targetObject.onmousemove = function handleMouseMove(event) {
-    event = event || window.event;
-  
-    var offX = 50 - (50*event.clientX)/(window.innerWidth/2);
-    var offY = 50 - (50*event.clientY)/(window.innerHeight/2);
-    // console.log(offY);
-
-    orbitOne.setAttribute("style", `left: ${50 - offX*1}%; top: ${50 - offY*1}%;`);
-    orbitTwo.setAttribute("style", `left: ${50 - offX*3}%; top: ${50 - offY*3}%;`);
-    orbitThree.setAttribute("style", `left: ${50 - offX*5}%; top: ${50 - offY*5}%;`);
-    orbitFour.setAttribute("style", `left: ${50 - offX*7}%; top: ${50 - offY*7}%;`);
-    orbitFive.setAttribute("style", `left: ${50 - offX*6}%; top: ${50 - offY*6}%;`);
-    orbitSix.setAttribute("style", `left: ${50 - offX*4}%; top: ${50 - offY*4}%;`);
-    orbitSeven.setAttribute("style", `left: ${50 - offX*5.5}%; top: ${50 - offY*5.5}%;`);
-    orbitEight.setAttribute("style", `left: ${50 - offX*8}%; top: ${50 - offY*8}%;`);
-    orbitNine.setAttribute("style", `left: ${50 - offX*(-1)}%; top: ${50 - offY*(-1)}%;`);
-
-    polyOne.setAttribute("style", `left: ${50 - offX*5.5}%; top: ${50 - offY*5.5}%;`)
-    polyFront.setAttribute("style", `left: ${50 - offX*9.5}%; top: ${50 - offY*9.5}%;`)
-
-    orbitFront.setAttribute("style", `left: ${50 - offX*9}%; top: ${50 - offY*9}%;`);
-
-    posX.textContent = `_${event.clientX}`;
-    posY.textContent = `_${event.clientY}`;
-
-    path.style.stroke = "rgb(97, 173, 235)";
-    path.setAttribute("d", `M0 2 L0 2`);
-    pauseInfo.textContent = "click to freeze";
-    
-    clearInterval(timer);
+  if (freezed) {
+    return
+  } else {
+    targetObject.onmousemove = handleMouseMove;
   }
 }
 
@@ -116,13 +123,18 @@ targetObject.onmouseleave = function removeEvent() {
     orbitNine,
     polyOne,
     polyFront,
-    orbitFront
+    orbitFront,
+    center
   ];
-  objectsArray.forEach(element => {
-    element.setAttribute("style", `left: 50%; top: 50%;`);
-  });
-  posX.textContent = "_000";
-  posY.textContent = "_000";
+  if (freezed) {
+    return
+  } else {
+    objectsArray.forEach(element => {
+      element.setAttribute("style", `left: 50%; top: 50%;`);
+    });
+    posX.textContent = "_000";
+    posY.textContent = "_000";
+  }
 }
 
 function showInfo() {
@@ -137,10 +149,6 @@ function hideInfo() {
 targetObject.onclick = function pauseAnimation() {
   var path = document.getElementById("timer-bar");
   var delay = 3000;
-  
-  path.setAttribute("d", `M0 2 L60 2`);
-  pauseInfo.textContent = "freezing";
-
   var objectsArray = [
     orbitOne,
     orbitTwo,
@@ -154,17 +162,35 @@ targetObject.onclick = function pauseAnimation() {
     polyOne,
     polyFront,
     orbitFront,
-
+    center
   ];
-  timer = setInterval(() => {
-    this.handle = objectsArray.forEach(element => {
-      path.style.stroke = "rgb(198, 230, 14)";
-      pauseInfo.textContent = "freeze";
-      element.style.animationPlayState = "paused";
-      showInfo();
-      clearInterval(timer);
+
+  if (freezed) {
+    path.setAttribute("d", `M0 2 L0 2`);
+    pauseInfo.textContent = "click to freeze";
+    path.style.stroke = "rgb(97, 173, 235)";
+    freezed = false;
+    targetObject.onmousemove = null;
+    objectsArray.forEach(element => {
+      element.style.animationPlayState = "running";
     });
-  }, delay);
+    hideInfo();
+  } else {
+    path.setAttribute("d", `M0 2 L60 2`);
+    pauseInfo.textContent = "freezing";
+
+    timer = setInterval(() => {
+      path.style.stroke = "rgb(198, 230, 14)";
+      pauseInfo.textContent = "freezed";
+      targetObject.onmousemove = null;
+      freezed = true;
+      showInfo();
+      this.handle = objectsArray.forEach(element => {
+        element.style.animationPlayState = "paused";
+      });
+      clearInterval(timer);
+    }, delay);
+  }
 }
 
 var pathTo = [];
@@ -195,9 +221,12 @@ navbar.children.item(0).onmouseover = function() {
 navbar.children.item(1).onmouseover = function() {
   underlinePath.setAttribute("d", `M${pathTo[1].left} 2 L${pathTo[1].right} 2`);
 };
-navbar.children.item(2).onmouseover = function() {
+
+function ulAbout() {
   underlinePath.setAttribute("d", `M${pathTo[2].left} 2 L${pathTo[2].right} 2`);
 };
+navbar.children.item(2).onmouseover = ulAbout;
+
 navbar.children.item(3).onmouseover = function() {
   underlinePath.setAttribute("d", `M${pathTo[3].left} 2 L${pathTo[3].right} 2`);
 };
@@ -206,7 +235,13 @@ function hideUnderlinePath() {
   underlinePath.setAttribute("d", `M${navbarWidth} 2 L${navbarWidth} 2`);
 }
 
-navbar.onmouseleave = hideUnderlinePath;
+navbar.onmouseleave = () => {
+  if (aboutActive) {
+    ulAbout();
+  } else {
+    hideUnderlinePath();
+  }
+};
 
 // About Me
 var aboutActive = false;
@@ -221,6 +256,8 @@ var strings = document.getElementsByClassName("string");
 
 var arrowHead = document.getElementById("arrow-head");
 var arrowStick = document.getElementById("arrow-stick");
+
+var dash = document.getElementById("dash-path");
 
 myName.onmouseover = function toArrow() {
   xPathTop.setAttribute("d", `M5 10 L10 15`);
@@ -253,11 +290,13 @@ function openAboutMe() {
   myName.style.opacity = 0;
   myName.onmouseleave = null;
 
+  closeSign.style.cursor = "pointer";
+
   xPathTop.setAttribute("d", `M5 5 L15 15`);
   xPathMid.setAttribute("d", `M10 10 L10 10`);
   xPathBottom.setAttribute("d", `M5 15 L15 5`);
 
-  showArrow();
+  dash.setAttribute("d", `M0 10 L170 10`);
 
   underlinePath.setAttribute("d", `M${pathTo[2].left} 2 L${pathTo[2].right} 2`);
 
@@ -265,15 +304,6 @@ function openAboutMe() {
     strings.item(p).style.bottom = "0px";
     strings.item(p).style.opacity = 1;
   }
-}
-
-function showArrow() {
-  arrowHead.setAttribute("d", `M25 25 L30 30 L25 35`);
-  arrowStick.setAttribute("d", `M10 30 L30 30`);
-}
-function hideArrow() {
-  arrowHead.setAttribute("d", `M25 30 L30 30 L25 30`);
-  arrowStick.setAttribute("d", `M30 30 L30 30`);
 }
 
 myName.onclick = openAboutMe;
@@ -300,6 +330,8 @@ function closeAboutMe() {
 
   underlinePath.setAttribute("d", `M${navbarWidth} 2 L${navbarWidth} 2`);
 
+  closeSign.style.cursor = "default";
+
   myName.style.fontFamily = "Rajdhani";
   myName.style.fontSize = "inherit";
   myName.style.left = "130px";
@@ -309,7 +341,7 @@ function closeAboutMe() {
   xPathTop.setAttribute("d", `M5 10 L10 10`);
   xPathBottom.setAttribute("d", `M10 10 L15 10`);
 
-  hideArrow();
+  dash.setAttribute("d", `M170 10 L170 10`);
 
   myName.onmouseleave = resetPathOnmouseLeave;
 
@@ -323,3 +355,8 @@ function closeAboutMe() {
 
 closeSign.onclick = closeAboutMe;
 
+var moreBtn = document.getElementById("more-btn");
+
+moreBtn.onmouseover = function moreBtnFX() {
+
+}
